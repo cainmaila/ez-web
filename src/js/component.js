af384948dd44693f207a8eb6218e75371689a228
Cain.Chu <cainmaila@gmail.com>
2016-10-13 20:14:41 +0800

    語系切換

--- src/js/component.js
+++ src/js/component.js
@@ -1,11 +1,13 @@
 var MyModal = require('./components/myModal/myModal.js');
 var MyModalBn = require('./components/myModalBn/myModalBn.vue');
+var EzHeader = require('./components/header/header.vue');
 
 module.exports.install = function (Vue, options) {
     Vue.mixin({
         beforeCreate: function () {
             Vue.component('ezModal', new MyModal('這一個Modal互動組件'));
             Vue.component('ezModalBn', MyModalBn);
+            Vue.component('ezHeader', EzHeader);
         }
     })
 }
---
+++ src/js/components/header/header.vue
@@ -0,0 +1,38 @@
+ <template>
+     <div class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                	<slot name="til">EZ Web App</slot>
+                </a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span>{{ $t("singup") }}</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{ $t("language") }}
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                          <li><a href="#" @click.prevent="change('en')">English</a></li>
+                          <li><a href="#" @click.prevent="change('zh-TW')">繁體中文</a></li>
+                          <li><a href="#" @click.prevent="change('ja')">日文</a></li> 
+                    </ul>
+                </li>
+            </ul>
+        </div>
+	</div>
+ </template>
+
+  <script>
+  	var Vue = require('vue');
+    module.exports = {
+        data: function () {
+            return {}
+        },
+        methods: {
+          change(lang){
+          	Vue.setLang(lang);
+          },
+        }
+    }
+ </script>
