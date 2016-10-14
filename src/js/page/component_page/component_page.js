1fcdda1a9a842bee4c74d9ae6bec116f087e54a3
Cain.Chu <cainmaila@gmail.com>
2016-10-14 19:39:39 +0800

    提示訊息

--- src/js/page/component_page/component_page.js
+++ src/js/page/component_page/component_page.js
@@ -1,4 +1,21 @@
 var template = require('./component_page.html');
 module.exports = {
     template: template,
+    data() {
+        return {
+            infoLine: false,
+        }
+    },
+    methods: {
+        swichInfoLine(f) {
+            if (f === 2) {
+                this.infoLine = !this.infoLine;
+            } else if (f === 0) {
+                this.infoLine = false;
+            } else {
+                this.infoLine = true;
+            }
+
+        }
+    }
 }
